71f9b6d0a9020779bfe0fb04f74e4eec679a0298
Danny McDonald <daniel.mcdonald@bitpanda.com>
2019-03-19 21:05:21 +0100

    py3

--- mezzanine/generic/defaults.py
+++ mezzanine/generic/defaults.py
@@ -10,9 +10,6 @@ that are only read during startup shouldn't be editable, since changing
 them would require an application reload.
 """
 
-from __future__ import unicode_literals
-from future.builtins import range
-
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 
